f4751af8b645a799182d6e985a64283012b5df36
Anthony Mace <amace93@gmail.com>
2015-11-28 00:44:32 -0700

    got list adding using rootScope, not sure if this is good. Button still messed up

--- listsApp.js
+++ listsApp.js
@@ -1,11 +1,11 @@
 (function(angular, undefined) {
   angular.module('listsApp', ['ngMaterial']).controller('ListsController', ListsController);
 
-  function ListsController($scope, $mdDialog) {
+  function ListsController($rootScope, $scope, $mdDialog) {
     $scope.listName = "";
     $scope.listType;
 
-    $scope.wishLists = [
+    $rootScope.wishLists = [
       {type:'Christmas', name:'test', dateCreated:'11/23/2015', dateModified:'Today'},
       {type:'Birthday', name:'test', dateCreated:'11/23/2015', dateModified:'Today'},
       {type:'Christmas', name:'test', dateCreated:'11/23/2015', dateModified:'Today'},
@@ -15,7 +15,6 @@
     var parentEl = angular.element(document.body);
     var alert = $mdDialog.alert({
         parent: parentEl,
-        title: "Let's create a Wish List!",
         template:     '<md-dialog layout-padding>' +
                       '  <md-dialog-content class="createListForm">'+
                       '     <h2>Create a Wish List</h2>' + 
@@ -23,15 +22,18 @@
                       '       <label>Wish List Name</label>' + 
                       '       <input required ng-model="listName">' +
                       '     </md-input-container>' + 
-                      '     <md-list-item class="md-3-line md-no-proxy ng-scope" ng-repeat="list in wishLists">' +
-                      '       <div class="md-list-item-text">' +
-                      '         <h3>{{list.name}}</h3>' +
+                      '     <md-radio-group ng-model="listType" class="md-primary">' +
+                      '       <md-radio-button value="Birthday" class="md-primary formInput">Birthday</md-radio-button>' + 
+                      '       <md-radio-button value="Christmas" class="md-primary formInput">Christmas</md-radio-button>' + 
+                      '     </md-radio-group>' + 
                       '  </md-dialog-content>' +
+                      '  <md-dialog-actions>' +
+                      '     <md-button ng-click="addList()" class="md-primary">' +
+                      '       Create!' +
+                      '     </md-button>' +
+                      '  </md-dialog-actions>' +
                       '</md-dialog>',
         clickOutsideToClose: true,
-        locals: {
-           wishLists: $scope.wishLists
-        },
         controller: ListsController
     });
 
@@ -40,14 +42,16 @@
     };
 
     $scope.closeDialog = function() {
-      console.log("test");
+      console.log($scope.listType);
+      console.log($scope.listName);
       $mdDialog.hide(alert);
+
     }
 
     $scope.addList = function() {
-      $scope.$apply(function(){
-        wishLists.push({type:$scope.listType, name:$scope.listName, dateCreated: 'Test', dateModified: 'Test'});
-      });
+        $rootScope.wishLists.push({type:$scope.listType, name:$scope.listName, dateCreated: 'Test', dateModified: 'Test'});
+        $mdDialog.hide(alert);
+        console.log($scope.wishLists);
     }
   }
 })(angular);
