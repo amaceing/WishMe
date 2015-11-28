bf37602af8fb526cf9b63c8bbd5e0126ce192a0c
Anthony Mace <amace93@gmail.com>
2015-11-28 01:03:17 -0700

    added Lato and Niccone fonts

--- listsApp.js
+++ listsApp.js
@@ -18,20 +18,20 @@
         template:     '<md-dialog layout-padding>' +
                       '  <md-dialog-content class="createListForm">'+
                       '     <h2>Create a Wish List</h2>' + 
-                      '     <md-input-container class="md-block formInput">' + 
-                      '       <label>Wish List Name</label>' + 
-                      '       <input required ng-model="listName">' +
-                      '     </md-input-container>' + 
-                      '     <md-radio-group ng-model="listType" class="md-primary">' +
-                      '       <md-radio-button value="Birthday" class="md-primary formInput">Birthday</md-radio-button>' + 
-                      '       <md-radio-button value="Christmas" class="md-primary formInput">Christmas</md-radio-button>' + 
-                      '     </md-radio-group>' + 
+                      '     <form name="createListForm">' +
+                      '       <md-input-container class="md-block formInput">' + 
+                      '         <label>Wish List Name</label>' + 
+                      '         <input name="listName" ng-model="listName">' +
+                      '       </md-input-container>' + 
+                      '       <md-radio-group ng-model="listType" class="md-primary">' +
+                      '         <md-radio-button value="Birthday" class="md-primary formInput">Birthday</md-radio-button>' +
+                      '         <md-radio-button value="Christmas" class="md-primary formInput">Christmas</md-radio-button>' + 
+                      '       </md-radio-group>' + 
+                      '       <md-button ng-click="addList()" class="md-primary">' +
+                      '         Create!' +
+                      '       </md-button>' +
+                      '     </form>' + 
                       '  </md-dialog-content>' +
-                      '  <md-dialog-actions>' +
-                      '     <md-button ng-click="addList()" class="md-primary">' +
-                      '       Create!' +
-                      '     </md-button>' +
-                      '  </md-dialog-actions>' +
                       '</md-dialog>',
         clickOutsideToClose: true,
         controller: ListsController
@@ -51,7 +51,6 @@
     $scope.addList = function() {
         $rootScope.wishLists.push({type:$scope.listType, name:$scope.listName, dateCreated: 'Test', dateModified: 'Test'});
         $mdDialog.hide(alert);
-        console.log($scope.wishLists);
     }
   }
 })(angular);
