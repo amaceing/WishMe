08aa7fe8d4842fe1ea5b2385351cf09d0867625e
Anthony Mace <amace93@gmail.com>
2015-11-28 00:21:44 -0700

    reorganized angular controller, got mdDialog working by reading docs on angular material ui

--- listsApp.js
+++ listsApp.js
@@ -1,6 +1,7 @@
-angular.module('listsApp', ['ngMaterial']).controller('ListsController', function($scope, $mdDialog) {
+(function(angular, undefined) {
+  angular.module('listsApp', ['ngMaterial']).controller('ListsController', ListsController);
 
-    // var wishList = this;
+  function ListsController($scope, $mdDialog) {
     $scope.listName = "";
     $scope.listType;
 
@@ -11,26 +12,28 @@ angular.module('listsApp', ['ngMaterial']).controller('ListsController', functio
       {type:'Birthday', name:'test', dateCreated:'11/23/2015', dateModified:'Today'}
     ];
 
+    var parentEl = angular.element(document.body);
     var alert = $mdDialog.alert({
+        parent: parentEl,
         title: "Let's create a Wish List!",
-        template: '<md-content class="createListForm" ng-controller="ListsController" flex layout-padding>' + 
-                    '<h2>Create a Wish List</h2>' + 
-                    '<md-input-container class="md-block formInput">' + 
-                      '<label>Wish List Name</label>' + 
-                      '<input required ng-model="listName">' +
-                    '</md-input-container>' + 
-                    '<md-radio-group class="formInput" ng-model="listType">' +
-                      '<md-radio-button value="Birthday" class="md-primary">Birthday</md-radio-button>' +
-                      '<md-radio-button value="Christmas" class="md-primary">Christmas</md-radio-button>' +
-                    '</md-radio-group>' +
-                    '<md-dialog-actions>' +
-                      '<md-button ng-click="addList()" class="md-primary">' +
-                        'Create!' +
-                      '</md-button>' +
-                    '</md-dialog-actions>' + 
-                  '</md-content>',
-        clickOutsideToClose: true
-      });
+        template:     '<md-dialog layout-padding>' +
+                      '  <md-dialog-content class="createListForm">'+
+                      '     <h2>Create a Wish List</h2>' + 
+                      '     <md-input-container class="md-block formInput">' + 
+                      '       <label>Wish List Name</label>' + 
+                      '       <input required ng-model="listName">' +
+                      '     </md-input-container>' + 
+                      '     <md-list-item class="md-3-line md-no-proxy ng-scope" ng-repeat="list in wishLists">' +
+                      '       <div class="md-list-item-text">' +
+                      '         <h3>{{list.name}}</h3>' +
+                      '  </md-dialog-content>' +
+                      '</md-dialog>',
+        clickOutsideToClose: true,
+        locals: {
+           wishLists: $scope.wishLists
+        },
+        controller: ListsController
+    });
 
     $scope.showAlert = function(ev) {
       $mdDialog.show(alert);
@@ -46,30 +49,5 @@ angular.module('listsApp', ['ngMaterial']).controller('ListsController', functio
         wishLists.push({type:$scope.listType, name:$scope.listName, dateCreated: 'Test', dateModified: 'Test'});
       });
     }
-
-    // var todoList = this;
-    // todoList.todos = [
-    //   {text:'learn angular', done:true},
-    //   {text:'build an angular app', done:false}];
- 
-    // todoList.addTodo = function() {
-    //   todoList.todos.push({text:todoList.todoText, done:false});
-    //   todoList.todoText = '';
-    // };
- 
-    // todoList.remaining = function() {
-    //   var count = 0;
-    //   angular.forEach(todoList.todos, function(todo) {
-    //     count += todo.done ? 0 : 1;
-    //   });
-    //   return count;
-    // };
- 
-    // todoList.archive = function() {
-    //   var oldTodos = todoList.todos;
-    //   todoList.todos = [];
-    //   angular.forEach(oldTodos, function(todo) {
-    //     if (!todo.done) todoList.todos.push(todo);
-    //   });
-    // };
-  });
+  }
+})(angular);
