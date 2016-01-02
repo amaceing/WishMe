bd069d86918cc2e22c3cd15b13c382c805bb0056
Anthony Mace <amace93@gmail.com>
2016-01-02 00:44:06 -0700

    implementing adding items to wishlists

--- listsApp.js
+++ listsApp.js
@@ -3,6 +3,8 @@
   angular.module('listsApp', ['ngMaterial', 'ngMessages']).controller('ListsController', ListsController);
 
   function ListsController($scope, $mdDialog) {
+
+    //list
     $scope.listType = "";
     $scope.listName = "";
     $scope.errorOnRequired = false;
@@ -10,6 +12,11 @@
     $scope.listSelected = false;
     $scope.selectedList = {};
 
+    //item
+    $scope.itemName = "";
+    $scope.itemPrice = "";
+
+    //List prototypes
     $scope.wishLists = [
       {
         type:'Christmas', name:'Julissa\'s Christmas', dateCreated:'11/23/2015', dateModified:'Today', 
@@ -29,12 +36,90 @@
       }
     ];
 
+    //selecting list
     $scope.selectList = function($list) {
       $scope.listSelected = true;
       $scope.selectedList = $list;
       console.log($list);
     }
 
+    $scope.addItemToList = function() {
+      console.log("test");
+    }
+
+    $scope.openMenu = function($mdOpenMenu, ev) {
+      var originatorEv = ev;
+      $mdOpenMenu(ev);
+    }
+
+    //dialog for adding item to list
+    $scope.showAddItemDialog = function($event) {
+      console.log("test");
+      var parentEl = angular.element(document.body);
+      $mdDialog.show({
+        parent: parentEl,
+        targetEvent : $event,
+        template:     '<md-dialog layout-padding>' +
+                      '  <md-dialog-content class="createListForm">'+
+                      '     <h2>Add Item</h2>' +
+                      '     <form name="addItemForm">' +
+                      '       <div layout-gt-sm="row">' +
+                      '         <md-input-container class="md-block formInput" flex-gt-sm>' + 
+                      '           <label>Item Name</label>' + 
+                      '           <input required name="itemName" ng-model="itemName">' +
+                      '           <div ng-if="$scope.errorOnRequired">' +
+                      '             <div ng-message="required">This is required.</div>' +
+                      '           </div>' +
+                      '         </md-input-container>' + 
+                      '         <md-input-container class="md-block formInput" flex-gt-sm>' + 
+                      '           <label>Item Price</label>' + 
+                      '           <input required type="number" step="0.01" name="itemPrice" ng-model="itemPrice">' +
+                      '           <div ng-if="$scope.errorOnRequired">' +
+                      '             <div ng-message="required">This is required.</div>' +
+                      '           </div>' +
+                      '         </md-input-container>' + 
+                      '         </div>' +
+                      '       <md-radio-group ng-model="listType" class="md-primary">' +
+                      '         <md-radio-button value="Birthday" class="md-primary formInput">Birthday</md-radio-button>' +
+                      '         <md-radio-button value="Christmas" class="md-primary formInput">Christmas</md-radio-button>' + 
+                      '       </md-radio-group>' + 
+                      '       <md-button ng-click="test()" class="md-primary">' +
+                      '         Create!' +
+                      '       </md-button>' +
+                      '     </form>' + 
+                      '  </md-dialog-content>' +
+                      '</md-dialog>',
+        clickOutsideToClose: true,
+        locals: {
+          itemName: $scope.itemName,
+          itemPrice: $scope.itemPrice
+        },
+        controller: AddItemController
+      });
+
+      function AddItemController($scope, itemPrice) {
+        $scope.itemPrice = itemPrice;
+
+        $scope.test = function() {
+          console.log($scope.itemPrice);
+        }
+
+        $scope.closeDialog = function() {
+          $mdDialog.hide();
+        }
+
+        $scope.addList = function() {
+          if ($scope.listName == "") {
+            $scope.errorOnRequired = true;
+          } else {
+            $scope.wishLists.push({type: $scope.listType, name: $scope.listName, dateCreated: '12/31/2015', dateModified: 'Yesterday'});
+            $scope.closeDialog();
+          }
+        }
+      }
+    }
+
+    //dialog for adding list
     function showAddListDialog($event) {
       var parentEl = angular.element(document.body);
       $mdDialog.show({
