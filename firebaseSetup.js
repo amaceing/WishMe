ad956f46ef5f17429c27204642e5b917038a415a
Anthony Mace <amace93@gmail.com>
2017-02-23 21:04:33 -0700

    added firebase setup script

---
+++ firebaseSetup.js
@@ -0,0 +1,8 @@
+var config = {
+	apiKey: "AIzaSyDPxiKe1_HNjdOd88SS4hJ3oQ3a28KOQ6M",
+	authDomain: "wishme-492f7.firebaseapp.com",
+	databaseURL: "https://wishme-492f7.firebaseio.com",
+	storageBucket: "wishme-492f7.appspot.com",
+	messagingSenderId: "600766901014"
+};
+firebase.initializeApp(config);
