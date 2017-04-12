ec6bb9876025b06ecf524d34de44162cb2c4ca92
Anthony Mace <amace93@gmail.com>
2017-04-11 22:07:45 -0700

    set up scope vars for email and password, login function in controller

--- JS/loginApp.js
+++ JS/loginApp.js
@@ -1,4 +1,13 @@
 (function(angular, undefined) {
-  "use strict";
-  angular.module('loginApp', ['ngMaterial', 'ngMessages']).controller('LoginController', LoginController);
+    "use strict";
+    angular.module('loginApp', ['ngMaterial', 'ngMessages']).controller('LoginController', LoginController);
+
+    function LoginController($scope) {
+        $scope.email = "";
+        $scope.password = "";
+
+        $scope.logUserIn = function() {
+            console.log("test");
+        }
+    }
 })(angular);
