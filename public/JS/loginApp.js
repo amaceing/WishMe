f86334e0f4d9638e362f442fb7bb556662213375
Anthony Mace <amace93@gmail.com>
2017-04-11 22:34:52 -0700

    registration for users

--- public/JS/loginApp.js
+++ public/JS/loginApp.js
@@ -2,16 +2,48 @@
     "use strict";
     angular.module('loginApp', ['ngMaterial', 'ngMessages']).controller('LoginController', LoginController);
 
-    function LoginController($scope) {
+    function LoginController($scope, $mdDialog) {
         $scope.email = "";
         $scope.password = "";
-
-        $scope.registerUser() = function() {
-            console.log("test2");
-        }
 
         $scope.logUserIn = function() {
             console.log("test");
         }
+
+		$scope.showRegisterUserDialog = function ($event) {
+			var parentEl = angular.element(document.body);
+			$mdDialog.show({
+				parent: parentEl,
+				targetEvent: $event,
+				templateUrl: 'HTML/registerUserDialog.html',
+				clickOutsideToClose: true,
+				controller: RegisterUserController
+			});
+
+			function RegisterUserController($scope) {
+				$scope.email = "";
+                $scope.password = "";
+
+                $scope.registerUser = function() {
+                    console.log($scope.email + " " + $scope.password);
+                    firebase.auth().createUserWithEmailAndPassword($scope.email, $scope.password).catch(function(error) {
+                        // Handle Errors here.
+                        var errorCode = error.code;
+                        var errorMessage = error.message;
+                        if (errorCode == 'auth/weak-password') {
+                            alert('The password is too weak.');
+                        } else {
+                            alert(errorMessage);
+                        }
+                        console.log(error);
+                    });
+                    $mdDialog.hide();
+                }
+
+                $scope.closeDialog = function () {
+					$mdDialog.hide();
+				}
+			}
+		}
     }
 })(angular);
