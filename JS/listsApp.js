18a01e152bbb37e14c7fe75fff8e84b69d5214c2
Anthony Mace <amace93@gmail.com>
2017-04-11 21:30:16 -0700

    setting up login controller

--- JS/listsApp.js
+++ JS/listsApp.js
@@ -1,184 +1,180 @@
-(function(angular, undefined) {
-  "use strict";
-  angular.module('listsApp', ['ngMaterial', 'ngMessages']).controller('ListsController', ListsController);
+(function (angular, undefined) {
+	"use strict";
+	angular.module('listsApp', ['ngMaterial', 'ngMessages']).controller('ListsController', ListsController);
 
-  function ListsController($scope, $mdDialog) {
+	function ListsController($scope, $mdDialog) {
 
-    //list
-    $scope.listType = "";
-    $scope.listName = "";
-    $scope.errorOnRequired = false;
-    $scope.showAddListDialog = showAddListDialog;
-    $scope.listSelected = false;
-    $scope.selectedList = {};
+		//list
+		$scope.listType = "";
+		$scope.listName = "";
+		$scope.errorOnRequired = false;
+		$scope.showAddListDialog = showAddListDialog;
+		$scope.listSelected = false;
+		$scope.selectedList = {};
 
-    //item
-    $scope.itemName;
-    $scope.itemPrice;
+		//item
+		$scope.itemName;
+		$scope.itemPrice;
 
-    //List prototypes
-    $scope.wishLists = [
-      new WishList(
-        'Julissa\'s Christmas', 
-        'Christmas', 
-        '11/23/2015', 
-        '06/04/2016',
-        [
-          new WishListItem('Rocket League', 9.99, new Date()),
-          new WishListItem('Rocket League', 9.99, new Date()),
-          new WishListItem('Rocket League', 9.99, new Date()),
-          new WishListItem('Rocket League', 9.99, new Date()),
-          new WishListItem('Rocket League', 9.99, new Date()),
-          new WishListItem('Rocket League', 9.99, new Date()),
-          new WishListItem('Rocket League', 9.99, new Date()),
-          new WishListItem('Rocket League', 9.99, new Date()),
-          new WishListItem('Rocket League', 9.99, new Date()),
-          new WishListItem('Rocket League', 9.99, new Date()),
-          new WishListItem('Rocket League', 9.99, new Date()),
-          new WishListItem('Rocket League', 9.99, new Date())
-        ]
-      ),
-      new WishList('Julissa\'s Christmas', 
-        'Christmas', 
-        '11/23/2015', 
-        '06/04/2016',
-        [
-          new WishListItem('Rocket League', 9.99, new Date()),
-          new WishListItem('Rocket League', 9.99, new Date()),
-          new WishListItem('Rocket League', 9.99, new Date()),
-          new WishListItem('Rocket League', 9.99, new Date()),
-          new WishListItem('Rocket League', 9.99, new Date()),
-          new WishListItem('Rocket League', 9.99, new Date())
-        ]
-      ),
-      new WishList('Julissa\'s Christmas', 
-        'Christmas', 
-        '11/23/2015', 
-        '06/04/2016',
-        [
-          new WishListItem('Rocket League', 9.99, new Date()),
-          new WishListItem('Rocket League', 9.99, new Date()),
-          new WishListItem('Rocket League', 9.99, new Date()),
-          new WishListItem('Rocket League', 9.99, new Date())
-        ]
-      ),
-      new WishList(
-        'Julissa\'s Christmas', 
-        'Christmas', 
-        '11/23/2015', 
-        '06/04/2016',
-        [
-          new WishListItem('Rocket League', 9.99, new Date()),
-          new WishListItem('Rocket League', 9.99, new Date()),
-          new WishListItem('Rocket League', 9.99, new Date()),
-          new WishListItem('Rocket League', 9.99, new Date()),
-          new WishListItem('Rocket League', 9.99, new Date()),
-          new WishListItem('Rocket League', 9.99, new Date()),
-          new WishListItem('Rocket League', 9.99, new Date()),
-          new WishListItem('Rocket League', 9.99, new Date()),
-          new WishListItem('Rocket League', 9.99, new Date()),
-          new WishListItem('Rocket League', 9.99, new Date())
-        ]
-      ),
-    ];
+		//List prototypes
+		$scope.wishLists = [
+			new WishList(
+				'Julissa\'s Christmas',
+				'Christmas',
+				'11/23/2015',
+				'06/04/2016', [
+					new WishListItem('Rocket League', 9.99, new Date()),
+					new WishListItem('Rocket League', 9.99, new Date()),
+					new WishListItem('Rocket League', 9.99, new Date()),
+					new WishListItem('Rocket League', 9.99, new Date()),
+					new WishListItem('Rocket League', 9.99, new Date()),
+					new WishListItem('Rocket League', 9.99, new Date()),
+					new WishListItem('Rocket League', 9.99, new Date()),
+					new WishListItem('Rocket League', 9.99, new Date()),
+					new WishListItem('Rocket League', 9.99, new Date()),
+					new WishListItem('Rocket League', 9.99, new Date()),
+					new WishListItem('Rocket League', 9.99, new Date()),
+					new WishListItem('Rocket League', 9.99, new Date())
+				]
+			),
+			new WishList('Julissa\'s Christmas',
+				'Christmas',
+				'11/23/2015',
+				'06/04/2016', [
+					new WishListItem('Rocket League', 9.99, new Date()),
+					new WishListItem('Rocket League', 9.99, new Date()),
+					new WishListItem('Rocket League', 9.99, new Date()),
+					new WishListItem('Rocket League', 9.99, new Date()),
+					new WishListItem('Rocket League', 9.99, new Date()),
+					new WishListItem('Rocket League', 9.99, new Date())
+				]
+			),
+			new WishList('Julissa\'s Christmas',
+				'Christmas',
+				'11/23/2015',
+				'06/04/2016', [
+					new WishListItem('Rocket League', 9.99, new Date()),
+					new WishListItem('Rocket League', 9.99, new Date()),
+					new WishListItem('Rocket League', 9.99, new Date()),
+					new WishListItem('Rocket League', 9.99, new Date())
+				]
+			),
+			new WishList(
+				'Julissa\'s Christmas',
+				'Christmas',
+				'11/23/2015',
+				'06/04/2016', [
+					new WishListItem('Rocket League', 9.99, new Date()),
+					new WishListItem('Rocket League', 9.99, new Date()),
+					new WishListItem('Rocket League', 9.99, new Date()),
+					new WishListItem('Rocket League', 9.99, new Date()),
+					new WishListItem('Rocket League', 9.99, new Date()),
+					new WishListItem('Rocket League', 9.99, new Date()),
+					new WishListItem('Rocket League', 9.99, new Date()),
+					new WishListItem('Rocket League', 9.99, new Date()),
+					new WishListItem('Rocket League', 9.99, new Date()),
+					new WishListItem('Rocket League', 9.99, new Date())
+				]
+			),
+		];
 
-    //selecting list
-    $scope.selectList = function($list) {
-      $scope.listSelected = true;
-      $scope.selectedList = $list;
-      console.log($list);
-    }
+		//selecting list
+		$scope.selectList = function ($list) {
+			$scope.listSelected = true;
+			$scope.selectedList = $list;
+			console.log($list);
+		}
 
-    $scope.deletion = function() {
-      $('.md-2-line').hover(
-        function() {
-          $(this).css("background-color", "#F2DEDE");
-        },
-        function() {
-          $(this).css("background-color", "white");
-        }
-      )
-    }
+		$scope.deletion = function () {
+			$('.md-2-line').hover(
+				function () {
+					$(this).css("background-color", "#F2DEDE");
+				},
+				function () {
+					$(this).css("background-color", "white");
+				}
+			)
+		}
 
 
-    $scope.openMenu = function($mdOpenMenu, ev) {
-      var originatorEv = ev;
-      $mdOpenMenu(ev);
-    }
+		$scope.openMenu = function ($mdOpenMenu, ev) {
+			var originatorEv = ev;
+			$mdOpenMenu(ev);
+		}
 
-    //dialog for adding item to list
-    $scope.showAddItemDialog = function($event) {
-      var parentEl = angular.element(document.body);
-      $mdDialog.show({
-        parent: parentEl,
-        targetEvent : $event,
-        templateUrl: 'HTML/addItemDialog.html',
-        clickOutsideToClose: true,
-        locals: {
-          selectedList: $scope.selectedList,
-          itemName: $scope.itemName,
-          itemPrice: $scope.itemPrice
-        },
-        controller: AddItemController
-      });
+		//dialog for adding item to list
+		$scope.showAddItemDialog = function ($event) {
+			var parentEl = angular.element(document.body);
+			$mdDialog.show({
+				parent: parentEl,
+				targetEvent: $event,
+				templateUrl: 'HTML/addItemDialog.html',
+				clickOutsideToClose: true,
+				locals: {
+					selectedList: $scope.selectedList,
+					itemName: $scope.itemName,
+					itemPrice: $scope.itemPrice
+				},
+				controller: AddItemController
+			});
 
-      function AddItemController($scope, selectedList, itemName, itemPrice) {
-        $scope.selectedList = selectedList;
-        $scope.itemName = itemName;
-        $scope.itemPrice = itemPrice;
+			function AddItemController($scope, selectedList, itemName, itemPrice) {
+				$scope.selectedList = selectedList;
+				$scope.itemName = itemName;
+				$scope.itemPrice = itemPrice;
 
-        $scope.addItem = function() {
-          console.log($scope.itemName);
-          console.log($scope.itemPrice);
-          var newItem = new WishListItem($scope.itemName, $scope.itemPrice, new Date());
-          $scope.selectedList.wishListItems.unshift(newItem);
-          $scope.closeDialog();
-        }
+				$scope.addItem = function () {
+					console.log($scope.itemName);
+					console.log($scope.itemPrice);
+					var newItem = new WishListItem($scope.itemName, $scope.itemPrice, new Date());
+					$scope.selectedList.wishListItems.unshift(newItem);
+					$scope.closeDialog();
+				}
 
-        $scope.closeDialog = function() {
-          $mdDialog.hide();
-        }
-      }
-    }
+				$scope.closeDialog = function () {
+					$mdDialog.hide();
+				}
+			}
+		}
 
-    //dialog for adding list
-    function showAddListDialog($event) {
-      var parentEl = angular.element(document.body);
-      $mdDialog.show({
-        parent: parentEl,
-        targetEvent : $event,
-        templateUrl: 'HTML/addListDialog.html',
-        clickOutsideToClose: true,
-        locals: {
-          lists: $scope.wishLists,
-          listName: $scope.listName,
-          reqError: $scope.errorOnRequired,
-          listType: $scope.listType
-        },
-        controller: AddListController
-      });
+		//dialog for adding list
+		function showAddListDialog($event) {
+			var parentEl = angular.element(document.body);
+			$mdDialog.show({
+				parent: parentEl,
+				targetEvent: $event,
+				templateUrl: 'HTML/addListDialog.html',
+				clickOutsideToClose: true,
+				locals: {
+					lists: $scope.wishLists,
+					listName: $scope.listName,
+					reqError: $scope.errorOnRequired,
+					listType: $scope.listType
+				},
+				controller: AddListController
+			});
 
-      function AddListController($scope, $mdDialog, lists, listName, reqError, listType) {
-        $scope.wishLists = lists;
-        $scope.listName = listName;
-        $scope.errorOnRequired = reqError;
-        $scope.listType = listType;
+			function AddListController($scope, $mdDialog, lists, listName, reqError, listType) {
+				$scope.wishLists = lists;
+				$scope.listName = listName;
+				$scope.errorOnRequired = reqError;
+				$scope.listType = listType;
 
-        $scope.closeDialog = function() {
-          $mdDialog.hide();
-        }
+				$scope.closeDialog = function () {
+					$mdDialog.hide();
+				}
 
-        $scope.addList = function() {
-          if ($scope.listName == "") {
-            $scope.errorOnRequired = true;
-          } else {
-            var newWishList = new WishList($scope.listName, $scope.listType, '12/31/2015', '06/03/2016', []);
-            $scope.wishLists.push(newWishList);
-            newWishList.saveList();
-            $scope.closeDialog();
-          }
-        }
-      }
-    }
-  }
+				$scope.addList = function () {
+					if ($scope.listName == "") {
+						$scope.errorOnRequired = true;
+					} else {
+						var newWishList = new WishList($scope.listName, $scope.listType, '12/31/2015', '06/03/2016', []);
+						$scope.wishLists.push(newWishList);
+						newWishList.saveList();
+						$scope.closeDialog();
+					}
+				}
+			}
+		}
+	}
 })(angular);
---
+++ JS/loginApp.js
@@ -0,0 +1,4 @@
+(function(angular, undefined) {
+  "use strict";
+  angular.module('loginApp', ['ngMaterial', 'ngMessages']).controller('LoginController', LoginController);
+})(angular);
